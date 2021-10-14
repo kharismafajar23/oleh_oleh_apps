923f0897ce25b41e73b6bd190f2e900a2bf68815
kharismafajar23 <70358725+kharismafajar23@users.noreply.github.com>
2021-10-14 15:26:02 +0700

    update kategori activity
    h3h3

--- app/src/main/java/com/demanganesia/oleh_oleh/KategoriActivity.java
+++ app/src/main/java/com/demanganesia/oleh_oleh/KategoriActivity.java
@@ -3,11 +3,13 @@ package com.demanganesia.oleh_oleh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class KategoriActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kategori);
     }
