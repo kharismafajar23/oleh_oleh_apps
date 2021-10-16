10b1ed1f44f918642420d90f496a029cf2d4b820
kharismafajar23 <70358725+kharismafajar23@users.noreply.github.com>
2021-10-16 08:59:13 +0700

    add

---
+++ app/src/main/java/com/demanganesia/oleh_oleh/JenisMakanan.java
@@ -0,0 +1,16 @@
+package com.demanganesia.oleh_oleh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class JenisMakanan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jenis_makanan);
+    }
+}
